3e9498097b1c6291ec81974cb9854b203be8a505
Steren <steren@google.com>
2022-06-21 02:19:26 -0700

    User journeys: update dependencies, print logs at every step and exits with error if replay fails (#21)
    * Update packages
    
    * Print log entries for all steps of user journey
    Fix user-journeys: add more log before each step #1919
    
    * Add comments
    
    * Look at resultof runner, exist with error if false

--- user-journeys/runner.js
+++ user-journeys/runner.js
@@ -1,7 +1,8 @@
 console.log('User journey runner is started.')
 
 import fs from 'fs';
-import { createRunner, parse } from '@puppeteer/replay';
+import puppeteer from 'puppeteer';
+import { createRunner, parse, PuppeteerRunnerExtension } from '@puppeteer/replay';
 
 const journeyFolder = './journeys';
 
@@ -36,12 +37,39 @@ if(taskIndex > replays.length) {
   process.exit(1);
 }
 
+// Create an extension that prints at every step of the replay
+class Extension extends PuppeteerRunnerExtension {
+  async beforeEachStep(step, flow) {
+    await super.beforeEachStep(step, flow);
+    console.log('Step: ', `${step.type} ${step.url || ''}`);
+  }
+
+  async afterAllSteps(flow) {
+    await super.afterAllSteps(flow);
+    console.log('All steps done');
+  }
+}
+
+// Start a browser and new page, needed to initialize the extension.
+// TODO: remove these lines if https://github.com/puppeteer/replay/issues/201 is fixed
+const browser = await puppeteer.launch({
+  headless: true,
+});
+const page = await browser.newPage();
+
 const recordingText = fs.readFileSync(`./journeys/${replays[taskIndex]}`, 'utf8');
 const recording = parse(JSON.parse(recordingText));
 
 console.log(`User journey ${taskIndex} running: ${replays[taskIndex]}`);
-const runner = await createRunner(recording);
-await runner.run();
-console.log(`User journey ${taskIndex} completed: ${replays[taskIndex]}`);
+const runner = await createRunner(recording, new Extension(browser, page, 7000));
+const result = await runner.run();
 
-console.log('User journey runner has finished.')
+if(result) {
+  console.log(`User journey ${taskIndex} completed successfully: ${replays[taskIndex]}`);
+  console.log('User journey runner has finished, exiting successfully')
+  process.exit();
+} else {
+  console.log(`User journey ${taskIndex} completed with errors: ${replays[taskIndex]}`);
+  console.log('User journey runner has finished, exiting with error')
+  process.exit(1);
+}
